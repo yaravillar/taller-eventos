742523ab0bd6422dd9726e3873ae8ce4330b2e89
yaravillar <yaravillardeoliveira@gmail.com>
2025-09-04 03:24:03 -0300

    uwu

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const div = document.querySelector("div");
+
+  div.addEventListener("click", function() {
+    alert("Hola! Soy el div");
+  });
+});
